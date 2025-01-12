007e8cbd34dce1f2c8f8aafa63a3c38004213af7
mohamed hosam mohamed <148141265+mohamedhosam4@users.noreply.github.com>
2025-01-12 02:53:56 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,14 +1,31 @@
 import helper
 import streamlit as st 
 import pickle
-model = pickle.load(open("D:/ODC/projct2/artifacts/lr.pkl",'rb'))
 
+# Load models
+models = {
+    "Logistic Regression": pickle.load(open("D:/ODC/projct2/artifacts/lr.pkl", 'rb')),
+    "SVM": pickle.load(open("D:/ODC/projct2/artifacts/svc.pkl", 'rb')),
+    "Decision Tree": pickle.load(open("D:/ODC/projct2/artifacts/dt.pkl", 'rb'))
+}
 
+# Allow user to select the desired model
+model_choice = st.selectbox("Select the model you want to use:", list(models.keys()))
+model = models[model_choice]
 
-text = st.text_input('enter your review')
+# User input for the review text
+text = st.text_input('Enter your review:')
 
-text = helper.text_preprocessing(text)
-pred = model.predict(text)
+# Process the input text
+if text:
+    processed_text = helper.text_preprocessing(text)
 
-if st.button('predict'):
-    st.text(pred)
+    # Make a prediction
+    if st.button('Predict'):
+        pred = model.predict(processed_text)[0]
+        
+        # Display appropriate message based on the prediction
+        if pred == 1:
+            st.success("The review is Positive!")
+        else:
+            st.error("The review is Negative!")
